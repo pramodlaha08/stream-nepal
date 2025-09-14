87f307d19269c8988a829f58337031b060a98e9c
Pramod Laha <pramodlaha@himalayacollege.edu.np>
2025-09-14 11:22:04 +0545

    feat(initialize): Initialized the nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function (){
+  return(
+    <>
+    <div>
+      <h1 className="text-3xl font-bold font-poppins text-center capitalize underline m-52 text-foreground">Welcome to streams nepal</h1>
+    </div>
+    </>
+  )
+}
