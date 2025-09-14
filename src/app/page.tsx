1512a4249763ca5be7f0afdd5dec94d55cd692ce
Pramod Laha <pramodlaha@himalayacollege.edu.np>
2025-09-14 13:34:37 +0545

    feat(homepage): add UpcomingTournaments component to the main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
 import ServicesShowcase from "../components/ServicesShowcase";
+import UpcomingTournaments from "../components/UpcomingTournaments";
 import AnimatedBackground from "../components/AnimatedBackground";
 
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
       <main className="relative z-10">
         <Hero />
         <ServicesShowcase />
+        <UpcomingTournaments />
       </main>
     </div>
   );
