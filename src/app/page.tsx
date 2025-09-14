2e1b0dbf36c32717dc87b0f96cb7ea9650cf129b
Pramod Laha <pramodlaha@himalayacollege.edu.np>
2025-09-14 15:46:50 +0545

    feat(homepage): add GamesSection component to the main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
 import ServicesShowcase from "../components/ServicesShowcase";
 import UpcomingTournaments from "../components/UpcomingTournaments";
+import GamesSection from "../components/GamesSection";
 import AnimatedBackground from "../components/AnimatedBackground";
 
 export default function HomePage() {
@@ -13,6 +14,7 @@ export default function HomePage() {
         <Hero />
         <ServicesShowcase />
         <UpcomingTournaments />
+        <GamesSection />
       </main>
     </div>
   );
