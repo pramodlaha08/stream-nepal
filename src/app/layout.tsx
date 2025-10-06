efb8f071897c3dec4f8ae8e759cd3064c00b1508
Pramod Laha <pramodlaha@himalayacollege.edu.np>
2025-10-06 14:07:01 +0545

    feat(Analytics): add Vercel Analytics integration for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 // Fonts
 const geistSans = Geist({
@@ -116,6 +117,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
