1a3407e80cc4c52b8706dafdbe336cdb267199b9
Pramod Laha <pramodlaha@himalayacollege.edu.np>
2025-11-05 14:16:12 +0545

    feat(config): add image remote patterns for Discord and other sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.discordapp.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.discord.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.discordapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.pngtree.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
